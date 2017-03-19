032ac62c5290b14d08acc446d0ed39b0786b09c8
mayur-rastogi <mayur.rastogi.iitkgp@gmail.com>
2017-03-19 21:12:39 +0530

    Mayur's version

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,10 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	matinv<-NULL
+	getmat<-function(){return(x)}
+	setmatinv<-function(inv){mi<<-inv}
+	getmatinv<-function(){return(mi)}
 }
 
 
@@ -12,4 +15,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	inv<-x$getmatinv()
+	if(!is.null(inv)){
+		print("Inverse exists in the Cache")
+		return(inv)
+	}
+	data<-x$getmat()
+	inv<-solve(data)
+	x$setmatinv(inv)
+	return(inv)
 }
